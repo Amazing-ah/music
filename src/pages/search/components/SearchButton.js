ae75a069d8def0318ce3181f990400068caeadf4
wang teng <wt1430@163.com>
2020-09-16 21:05:28 +0800

    9-16 music 搜索

--- src/pages/search/components/SearchButton.js
+++ src/pages/search/components/SearchButton.js
@@ -3,12 +3,11 @@ export default function SearchButton(props) {
   const { searchButton, reqSearch } = props;
   return (
     <div>
-      {/* TODO  点击按钮，取值，传给value */}
       <div className="searchButton">
         {searchButton.map((item) => {
           return (
             <div key={item.score} onClick={() => reqSearch(item.searchWord)}>
-              {item.searchWord}{" "}
+              {item.searchWord}
             </div>
           );
         })}
